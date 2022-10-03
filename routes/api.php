e2c99a13a9addff31f6d22b1b0aef1945769962f
Israel Cena <israelsjm@gmail.com>
2022-10-03 16:14:32 -0300

    include health-check get req

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/health-check', function () {
+    return response()->json(['success' => true,]);
+})->name('health-check');
 
 Route::get('/', function () {
     return response()->json(['success' => true,]);
