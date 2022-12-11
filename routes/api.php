838ab71218069aad69f61dfbca40d5dee99ec871
Israel Cena <israelsjm@gmail.com>
2022-12-11 14:02:12 -0300

    include auth in routes

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\{CourseController, LessonController, ModuleController, ReplySupportController, SupportController};
 use App\Http\Controllers\Auth\AuthController;
-use App\Models\ReplySupport;
 
 Route::get('/', function () {
     return response()->json([
@@ -11,34 +10,36 @@ Route::get('/', function () {
     ]);
 });
 
-Route::post('/auth', [AuthController::class, 'auth'])->name('auth');
-
 Route::get('/health-check', function () {
     return response()->json(['success' => true,]);
 })->name('health-check');
 
-Route::controller(CourseController::class)->group(function () {
-    Route::get('/courses', 'index')->name('courses');
-    Route::get('/courses/{id}', 'show')->name('courses.show');
-});
+Route::post('/auth', [AuthController::class, 'auth'])->name('auth');
 
-Route::get('/courses/{id}/modules', [ModuleController::class, 'showByCourse'])->name('courses.modules');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::controller(CourseController::class)->prefix('courses')->group(function () {
+        Route::get('/', 'index')->name('courses');
+        Route::get('/{id}', 'show')->name('courses.show');
+    });
 
-Route::controller(LessonController::class)->group(function () {
-    Route::get('/modules/{id}/lessons', 'showByLesson')->name('modules.lessons');
-    Route::get('/lesson/{id}', 'show')->name('lesson.show');
-});
+    Route::get('/courses/{id}/modules', [ModuleController::class, 'showByCourse'])->name('courses.modules');
 
-Route::controller(SupportController::class)->group(function () {
-    Route::get('/supports', 'index')->name('supports');
-    Route::post('/supports', 'store')->name('supports.store');
-    Route::get('/supports/me', 'showByUser')->name('support.user');
-    Route::get('/supports/{id}', 'show')->name('support.show');
-    Route::get('/lesson/{id}/supports', 'showByLesson')->name('lesson.supports');
-});
+    Route::controller(LessonController::class)->group(function () {
+        Route::get('/modules/{id}/lessons', 'showByLesson')->name('modules.lessons');
+        Route::get('/lesson/{id}', 'show')->name('lesson.show');
+    });
 
-Route::controller(ReplySupportController::class)->prefix('replies')->group(function () {
-    Route::get('/', 'index')->name('replies');
-    Route::post('/', 'store')->name('reply.store');
-    Route::get('/{id}', 'show')->name('replies.show');
+    Route::controller(SupportController::class)->group(function () {
+        Route::get('/supports', 'index')->name('supports');
+        Route::post('/supports', 'store')->name('supports.store');
+        Route::get('/supports/me', 'showByUser')->name('support.user');
+        Route::get('/supports/{id}', 'show')->name('support.show');
+        Route::get('/lesson/{id}/supports', 'showByLesson')->name('lesson.supports');
+    });
+
+    Route::controller(ReplySupportController::class)->prefix('replies')->group(function () {
+        Route::get('/', 'index')->name('replies');
+        Route::post('/', 'store')->name('reply.store');
+        Route::get('/{id}', 'show')->name('replies.show');
+    });
 });
